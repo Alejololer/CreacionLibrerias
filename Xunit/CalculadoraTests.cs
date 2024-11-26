3502fdd7ba6eae3bce7c634370ea4b0b7fbff1f1
Alejololer <acarlos29112003@gmail.com>
2024-11-26 12:26:45 -0500

    Cambios nuevos tests

--- Xunit/CalculadoraTests.cs
+++ Xunit/CalculadoraTests.cs
@@ -6,29 +6,94 @@ namespace Tests
     public class CalculadoraTests
     {
         [Fact]
-        public void Sumar_DosNumeros_RetornaResultadoCorrecto()
+        public void Sumar_NumerosNegativos_RetornaResultadoCorrecto()
         {
             // Arrange
             var calculadora = new Calculadora();
 
             // Act
-            var resultado = calculadora.Sumar(3, 2);
+            var resultado = calculadora.Sumar(-3, -2);
 
             // Assert
-            Assert.Equal(5, resultado);
+            Assert.Equal(-5, resultado);
         }
 
         [Fact]
-        public void Restar_DosNumeros_RetornaResultadoCorrecto()
+        public void Restar_NumerosNegativos_RetornaResultadoCorrecto()
         {
             // Arrange
             var calculadora = new Calculadora();
 
             // Act
-            var resultado = calculadora.Restar(5, 3);
+            var resultado = calculadora.Restar(-5, -3);
+
+            // Assert
+            Assert.Equal(-2, resultado);
+        }
+
+        [Fact]
+        public void Sumar_NumeroPositivoYNegativo_RetornaResultadoCorrecto()
+        {
+            // Arrange
+            var calculadora = new Calculadora();
+
+            // Act
+            var resultado = calculadora.Sumar(5, -3);
 
             // Assert
             Assert.Equal(2, resultado);
+        }
+
+        [Fact]
+        public void Restar_NumeroPositivoYNegativo_RetornaResultadoCorrecto()
+        {
+            // Arrange
+            var calculadora = new Calculadora();
+
+            // Act
+            var resultado = calculadora.Restar(5, -3);
+
+            // Assert
+            Assert.Equal(8, resultado);
+        }
+
+        [Fact]
+        public void Sumar_ConCero_RetornaElOtroNumero()
+        {
+            // Arrange
+            var calculadora = new Calculadora();
+
+            // Act
+            var resultado = calculadora.Sumar(0, 7);
+
+            // Assert
+            Assert.Equal(7, resultado);
+        }
+
+        [Fact]
+        public void Restar_ConCero_RetornaElOtroNumero()
+        {
+            // Arrange
+            var calculadora = new Calculadora();
+
+            // Act
+            var resultado = calculadora.Restar(0, 7);
+
+            // Assert
+            Assert.Equal(-7, resultado);
+        }
+
+        [Fact]
+        public void Restar_DosNumerosIguales_RetornaCero()
+        {
+            // Arrange
+            var calculadora = new Calculadora();
+
+            // Act
+            var resultado = calculadora.Restar(5, 5);
+
+            // Assert
+            Assert.Equal(0, resultado);
         }
     }
 }
