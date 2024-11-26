2236a693c92622f989b9e4e9c3ec3f93c8680eaf
Alejololer <acarlos29112003@gmail.com>
2024-11-26 12:20:33 -0500

    Primerr Commit

---
+++ CreaciónLibrerías/Calculadora.cs
@@ -0,0 +1,17 @@
+﻿namespace MiLibreria
+{
+    public class Calculadora
+    {
+        // Método para sumar dos números
+        public int Sumar(int a, int b)
+        {
+            return a + b;
+        }
+
+        // Método para restar dos números
+        public int Restar(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
